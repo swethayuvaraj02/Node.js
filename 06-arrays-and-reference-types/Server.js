bda81779aad620f90581224242e8dc464e18af33
swetha Y <yswetha38@gmail.com>
2026-07-21 11:57:16 +0530

    Delete 06-arrays-and-reference-types directory

--- 06-arrays-and-reference-types/Server.js
+++
@@ -1,9 +0,0 @@
-const http = require('http');
-
- const routes = require('./routes');
-
-const server = http.createServer(routes);
-
-server.listen(3000, () => {
-  console.log('Server running at http://localhost:3000');
-});
