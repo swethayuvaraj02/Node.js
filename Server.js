49235dc2da3539b06ec6abdd6768a8ac0edf0366
swetha Y <yswetha38@gmail.com>
2026-07-21 11:56:59 +0530

    Add files via upload

---
+++ Server.js
@@ -0,0 +1,9 @@
+const http = require('http');
+
+ const routes = require('./routes');
+
+const server = http.createServer(routes);
+
+server.listen(3000, () => {
+  console.log('Server running at http://localhost:3000');
+});
